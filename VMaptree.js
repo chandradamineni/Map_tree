e4023affb1514b711b0e85252f04f99a5706e360
chandradamineniui <chandu.ui99@gmail.com>
2017-05-26 15:35:32 -0500

    Update VMaptree.js

--- VMaptree.js
+++ VMaptree.js
@@ -211,3 +211,9 @@ Polymer({
         self.legendData = legendData;
     }
 });
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
